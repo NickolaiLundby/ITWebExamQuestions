2ac46964fee805b23b1391f38ab67b355333c9ad
Rune <runekeena@gmail.com>
2020-01-14 17:07:11 +0100

    Q4

---
+++ Q4/frontend/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Chat from './Chat'
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <ul>
+              <Link to="/chat" className="btn btn-link">Chat</Link>
+              <Link to="/test" className="btn btn-link">Test</Link>
+            </ul>
+          </header>
+        </div>
+        <Switch>
+          <Route path="/chat" component={Chat}/>
+          <Route path ="/test">
+            <p>Hej fra test</p>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+    )
+  }
+}
+
+export default App
