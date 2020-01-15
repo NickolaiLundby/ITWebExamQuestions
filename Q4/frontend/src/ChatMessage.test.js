3102a894db2fb27d97a4d8c32cf181f5e012fed8
Rune <runekeena@gmail.com>
2020-01-15 15:39:19 +0100

    jest

--- Q4/frontend/src/ChatMessage.test.js
+++ Q4/frontend/src/ChatMessage.test.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import ChatMessage from './ChatMessage';
+import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 
-describe('ChatMessage', () => {
+describe('<ChatMessage/>', () => {
     test('Renders', () => {
-        const mockChatMessage = {
-            name: 'Bob',
+        const mockChatmMssage = {
+            name: 'Bob', 
             message: 'Hello'
         };
-        const {getByText} = render(<ChatMessage name={mockChatMessage.name} message={mockChatMessage.message} />);
-        expect(getByText('Hello')).toBeInTheDocument();
+        const wrapper = shallow(<ChatMessage name={mockChatmMssage.name} message={mockChatmMssage.message} />);
         expect(wrapper.find('p').length).toBe(1);
-        expect(wrapper.find('p').text).toBe(mockChatMessage.name + " " + mockChatMessage.message);
-        //expect(wrapper.find('em').length).toBe(1);
-        expect(wrapper.find('em').text).toBe(mockChatMessage.message);
+        expect(wrapper.find('p').text()).toBe(mockChatmMssage.name + " " + mockChatmMssage.message);
+        expect(wrapper.find('strong').length).toBe(1);
+        expect(wrapper.find('strong').text()).toBe(mockChatmMssage.name);
+        expect(wrapper.find('em').length).toBe(1);
+        expect(wrapper.find('em').text()).toBe(mockChatmMssage.message);
     })
 })
